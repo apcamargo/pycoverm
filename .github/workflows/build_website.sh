c7ef328a898a96731bfe473450d9734f6c6e5ddf
Antonio Camargo <antoniop.camargo@gmail.com>
2020-10-03 20:40:32 -0300

    Fix workflow for Ubuntu

--- .github/workflows/build_website.sh
+++
@@ -1,7 +0,0 @@
-# Abort on errors
-set -e
-
-# Install yarn and build website
-yarn global add vuepress@1.6.0
-yarn add -D maginapp/vuepress-plugin-katex markdown-it-footnote
-yarn --cwd ./website website:build
