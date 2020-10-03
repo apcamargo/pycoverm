452fcd79f6a31a92f12c8f96f82fec3294f0a8fd
Antonio Camargo <antoniop.camargo@gmail.com>
2020-10-03 20:42:34 -0300

    'apt install' is now sudoed

--- .github/workflows/install_dependencies.sh
+++ .github/workflows/install_dependencies.sh
@@ -4,7 +4,7 @@ set -e
 # If on Linux, install dependencies for rust-htslib
 case "$(uname -s)" in
     Linux)
-        apt install zlib1g-dev libbz2-dev liblzma-dev clang pkg-config
+        sudo apt install zlib1g-dev libbz2-dev liblzma-dev clang pkg-config
     ;;
     *)
 esac
